2b8bfa244ebb90567860a8359908739a6fc1ab45
MinhUy999 <tpmudat1999@gmai.com>
2024-03-12 17:17:18 +0700

    btbuoi29

---
+++ models/Person.js
@@ -0,0 +1,83 @@
+export class Person {
+    constructor(id, type, name, address, email) {
+      this.id = id;
+      this.type = type;
+      this.name = name;
+      this.address = address;
+      this.email = email;
+    }
+  }
+  export class Student extends Person {
+    constructor(id, type, name, address, email, math, physical, chemistry) {
+      super(id, type, name, address, email);
+      this.math = math;
+      this.physical = physical;
+      this.chemistry = chemistry;
+    }
+    // hiển thị về chuỗi trả thông tin về điểm toán, lý, hóa
+    render() {
+      return `
+      <td>toan: ${this.math} ly: ${this.physical} hoa: ${
+        this.chemistry
+      }<p class="text-danger">diemTB: ${this.getAvg()}</p></td>`;
+    }
+    // tính điểm trung bình
+    getAvg() {
+      return (
+        (parseFloat(this.math) +
+          parseFloat(this.physical) +
+          parseFloat(this.chemistry)) /
+        3
+      ).toFixed(1);
+    }
+  }
+  
+  export class Employee extends Person {
+    constructor(id, type, name, address, email, workDays, dailySalary) {
+      super(id, type, name, address, email);
+      this.workDays = workDays;
+      this.dailySalary = dailySalary;
+    }
+    render() {
+      return `
+      <td>so ngay lam : ${this.workDays} luong : ${this.formatTien(
+        this.dailySalary
+      )} <p class="text-danger">Tinh luong: ${this.getSalary()}</p></td>`;
+    }
+    formatTien(money) {
+      let VND = new Intl.NumberFormat("vi-VN", {
+        style: "currency",
+        currency: "VND",
+      });
+      return VND.format(money);
+    }
+  
+    getSalary() {
+      return this.formatTien(
+        parseFloat(this.workDays) * parseFloat(this.dailySalary)
+      );
+    }
+  }
+  
+  export class Customer extends Person {
+    constructor(id, type, name, address, email, companyName, orderValue, rating) {
+      super(id, type, name, address, email);
+      this.companyName = companyName;
+      this.orderValue = orderValue;
+      this.rating = rating;
+    }
+    render() {
+      return `
+      <td>Ten Cty : ${this.companyName} , Gia Tri Hoa Don : ${this.formatTien(
+        this.orderValue
+      )} , Danh Gia: ${this.rating}</td>`;
+    }
+    formatTien(moneyHD) {
+      let VND = new Intl.NumberFormat("vi-VN", {
+        style: "currency",
+        currency: "VND",
+      });
+      return VND.format(moneyHD);
+    }
+  }
+  
---
+++ models/listperson.js
@@ -0,0 +1,59 @@
+export class ListPerson {
+    constructor() {
+      this.list = [];
+    }
+  
+    addPerson(Person) {
+      this.list.push(Person);
+    }
+  
+    removePerson(id) {
+      this.list = this.list.filter((Person) => Person.id !== id);
+    }
+    findPerson = (id) => {
+      const existedPerson = this.list.find((element) => {
+        return element.id === id;
+      });
+      return existedPerson;
+    };
+  
+    updatePerson = (Person) => {
+      const index = this.list.findIndex((element) => {
+        return element.id === Person.id;
+      });
+      this.list[index] = Person;
+    };
+    fillterByType = (type) => {
+      const data = this.list.filter((element) => {
+        if (type === "all") {
+          return true;
+        }
+        return element.type === type;
+      });
+      return data;
+    };
+    fillterByName = (name) => {
+      if (name === "AdenZ") {
+        let data = this.list.sort((a, b) => {
+          if (a.name.split("").pop() < b.name.split("").pop()) {
+            return -1;
+          } else if (a.name.split("").pop() > b.name.split("").pop()) {
+            return 1;
+          }
+          return 0;
+        });
+        return data;
+      } else if (name === "ZdenA") {
+        let data = this.list.sort((a, b) => {
+          if (a.name.split("").pop() > b.name.split("").pop()) {
+            return -1;
+          } else if (a.name.split("").pop() < b.name.split("").pop()) {
+            return 1;
+          }
+          return 0;
+        });
+        return data;
+      }
+    };
+  }
+  
